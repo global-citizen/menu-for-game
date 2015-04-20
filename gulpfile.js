9df3aed847423d5341d97450dd18a3b544b1bcaf
global-citizen <kainebone@Cl-ns-MacBook-Air.local>
2015-04-21 01:36:15 +0300

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
-    stylus = require('gulp-stylus');
+  jade = require('gulp-jade'),
+  stylus = require('gulp-stylus');
 
 
 gulp.task('html', function() {
 
-    gulp.src('./*.jade')
-        .pipe(jade({
-            pretty: true
-        }))
-        .pipe(gulp.dest('./build/'))
+  gulp.src('./*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build/'))
 });
 
 gulp.task('css', function () {
-    gulp.src('./stylesheets/main.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('./build/css/'));
+  gulp.src('./stylesheets/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./build/css/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./*.jade', ['html']);
-    gulp.watch('./stylesheets/main.styl', ['css']);
+  gulp.watch('./*.jade', ['html']);
+  gulp.watch('./stylesheets/main.styl', ['css']);
 });
 
 
 gulp.task('default', ['html', 'css', 'watch'], function() {
-    // place code for your default task here
+  // place code for your default task here
 });
